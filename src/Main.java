c640d7ef26ab221cbe30e7001cd66fcb5535e363
AK40084384 <akshayanand2121@gmail.com>
2026-01-27 22:12:26 -0800

    Update code

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("i = " + i);
         }
         System.out.println("Hello and welcome!");
-        System.out.println("Hello and welcome!");
+        System.out.println("welcome!");
 
 
     }
